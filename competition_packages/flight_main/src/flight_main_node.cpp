c47893bc0e131924789f918e2a6f41286fc9b83c
SurfaceNate <nathaniel_hamilton@outlook.com>
2018-05-11 17:33:51 -0500

    The skeleton of the main code has been written. It will be finished by tonight.

--- competition_packages/flight_main/src/flight_main_node.cpp
+++ competition_packages/flight_main/src/flight_main_node.cpp
@@ -26,6 +26,8 @@
 
 /* All of the helper functions are listed here, but are written out after the main function. */
 
+/* All of the callback functions are listed here, but are written after the helper functions. */
+
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
@@ -33,14 +35,34 @@ void state_cb(const mavros_msgs::State::ConstPtr& msg){
 
 int main(int argc, char **argv)
 {
-    /* Calculate the desired search route */
+	/* Setup all subscribers. */
+	//TODO: Figure out which ones are needed and subscribe to them
+	
+    /* Calculate the desired search route. */
 	//TODO: Write the function and put it here
 	
 	/* Wait for the RC controller to give up control. */
+	//TODO: Write code for this
 	
-	/* Take off */
+	/* Take off! */
+	//TODO: Make a function for this probably
 	
-	/* Follow the  */
+	/* Follow the route until the IP side says they found the marker. */
+	
+	/* Follow IP commands until there are no more updates. */
+	
+	
+	/* Pick up the marker. */
+	
+	
+	/* Fly to the drop off location. */
+	
+	
+	/* Execute drop off manuever. */
+	
+	/* Fly to landing location. */
+	
+	/* Land the drone. */
 	
 	
 }
