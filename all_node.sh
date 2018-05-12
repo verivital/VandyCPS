0edb6badc3feae74d727a2495302d5550a3625f7
RANHAOHR <rxh349@case.edu>
2018-05-12 18:12:58 -0500

    add vision node

---
+++ all_node.sh
@@ -0,0 +1,6 @@
+#bin/bash
+roscore&
+roslaunch realsense_camera r200_nodelet_rgbd.launch & 
+rosrun mavros mavros_node _fcu_url:=tcp://127.0.0.1:5760 _system_id:=2&
+rosrun cps_vision cps_vision&
+# rosrun flight_main flight_main_node
